362eb6ba3de77988c027f735b8fae8b487a56f96
CaGraciano <gracianocamila7@gmail.com>
2021-11-09 21:51:12 -0300

    criei e editei o index.js

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,48 @@
+//Exercícios de interpretação de código
+/* 1. 10 
+      10 5 
+ 2. 10 10 10
+ 3.let horasDeTrabalhoDiarias= prompt("Quantas horas você trabalha por dia?")
+let valorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`) */
+
+//Exercícios de escrita de código
+//Exercício 1
+let nome 
+let idade 
+console.log (typeof nome)
+console.log (typeof idade)
+// undefined
+let nomeUser = prompt ("Qual é o seu nome?")
+let idadeUser = prompt ("Qual é a sua idade?")
+console.log (typeof nomeUser)
+console.log (typeof idadeUser)
+//string
+//string
+console.log ("Olá", nome, "você tem", idade,"anos")
+
+//Exercício 2
+let corDaRoupa = ("Você está usando uma roupa azul hoje?") 
+let usaOculos = ("Você usa óculos?")
+let maiorDeIdade = ("Você tem mais de 18 anos?")
+let roupaAzul = "SIM"
+let usoOculos = "SIM"
+let maisDeDezoito = "SIM" 
+
+console.log (corDaRoupa + "-" +roupaAzul)
+console.log (usaOculos + "-" + usoOculos)
+console.log (maiorDeIdade + "-" + maisDeDezoito)
+
+//Exercício 3
+let a = 10
+let b = 25
+let c = a
+ a = b
+ b = c
+console.log ("O novo valor de a é" + " " + a)
+console.log ("O novo valor de b é" + " " + b)
+
+
+
+
+
