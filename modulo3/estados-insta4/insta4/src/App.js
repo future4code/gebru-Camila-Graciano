799dd4f410a75084309b6ff0a646ebbd36a02d0f
CaGraciano <gracianocamila7@gmail.com>
2022-02-04 16:19:48 -0300

    Correção de código

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -15,8 +15,37 @@ class App extends React.Component {
       { id: 1, nomeUsuario: 'paulinha', fotoUsuario: 'https://picsum.photos/50/50', fotoPost: 'https://picsum.photos/200/150' },
       { id: 2, nomeUsuario: 'Julio.c', fotoUsuario: 'https://picsum.photos/id/1009/50/50', fotoPost: 'https://picsum.photos/id/1004/200/150' },
       { id: 3, nomeUsuario: 'Zenaide', fotoUsuario: 'https://picsum.photos/id/1011/50/50', fotoPost: 'https://picsum.photos/id/102/200/150' }
-    ]
+    ],
+    valorInputUser: "",
+    valorInputFoto: "",
+    valorInputPost: ""
   }
+
+
+  adicionaPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.valorInputUser,
+      fotoUsuario: this.state.valorInputFoto,
+      fotoPost: this.state.valorInputPost
+    };
+
+    const novosPosts = [...this.state.postState, novoPost];
+
+    this.setState({ postState: novosPosts });
+    this.setState({     valorInputUser: "", valorInputFoto: "", valorInputPost: "" });
+  };
+
+  onChangeInputUser = (event) => {
+    this.setState({ valorInputUser: event.target.value });
+  };
+
+  onChangeInputFoto = (event) => {
+    this.setState({ valorInputFoto: event.target.value });
+  };
+
+  onChangeInputPost = (event) => {
+    this.setState({ valorInputPost: event.target.value });
+  };
 
 
   render() {
@@ -30,8 +59,25 @@ class App extends React.Component {
 
     return (
       <MainContainer>
-        {listaDePosts}
-      </MainContainer>
+      <input 
+      value={this.state.valorInputUser}
+      onChange={this.onChangeInputUser}
+      placeholder={"Seu Usuário"}
+    />
+    <input 
+      value={this.state.valorInputFoto}
+      onChange={this.onChangeInputFoto}
+      placeholder={"Link Foto de perfil"}
+    />
+    <input 
+      value={this.state.valorInputPost}
+      onChange={this.onChangeInputPost}
+      placeholder={"Foto do Post"}
+    />
+
+    <button onClick={this.adicionaPost}>Postar</button>
+    {listaDePosts}
+    </MainContainer>
     );
   }
 }
