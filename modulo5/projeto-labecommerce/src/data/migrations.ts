f7d1d8ebe006a54826a46ecdac324ebfb9f03b70
CaGraciano <gracianocamila7@gmail.com>
2022-07-05 20:23:19 -0300

    Endpoints

--- modulo5/projeto-labecommerce/src/data/migrations.ts
+++ modulo5/projeto-labecommerce/src/data/migrations.ts
@@ -9,24 +9,24 @@ const createTables = () => connection
    .raw(`
       CREATE TABLE IF NOT EXISTS labecommerce_users (
         id VARCHAR(255) PRIMARY KEY,
-        name VARCHAR(255) NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NULL
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
       );
 
       CREATE TABLE IF NOT EXISTS labecommerce_products (
-        id INT PRIMARY KEY AUTO_INCREMENT,
+        id VARCHAR(255) PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
-        price INT NOT NULL,
+        price float NOT NULL,
         image_url VARCHAR(255) NOT NULL
       );
 
       CREATE TABLE IF NOT EXISTS labecommerce_purchases (
         id VARCHAR(255) PRIMARY KEY,
         user_id VARCHAR(255) NOT NULL,
-        product_id INT NOT NULL,
+        product_id VARCHAR(255) NOT NULL,
         quantity INT NOT NULL,
-        total_price INT NOT NULL,
+        total_price float NOT NULL,
         FOREIGN KEY (user_id) REFERENCES labecommerce_users(id),
         FOREIGN KEY (product_id) REFERENCES labecommerce_products(id)
      );
@@ -54,4 +54,4 @@ const closeConnection = () => { connection.destroy() }
 createTables()
    .then(insertUsers)
    .then(insertProducts)
-   .finally(closeConnection)
+   .finally(closeConnection) 
