8ad79ccf87304e9f31b7351149b60399ebe14c61
CaGraciano <gracianocamila7@gmail.com>
2022-11-01 09:43:07 -0300

    Projeto Personare

---
+++ personare/src/components/Button/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+background-color: 	#FAFAD2;
+background-position: center;
+background-size: cover;
+padding: 10px 20px;
+font-size: 20px;
+border-radius: 10px;
+color: black;
+border: 5px solid black;
+font-weight: bold;
+:hover {
+  background-color: #F0E68C;
+  transform: scale(1.05);
+}
+`
